ff1dbbb7d6cae18661c27ef8d1bf9d372857bb3d
Jitu Rajak <83398258+FCJ6202@users.noreply.github.com>
2023-07-15 18:38:44 +0000

    Update NoteFunction.js

--- frontend/src/Components/NoteFunction.js
+++ frontend/src/Components/NoteFunction.js
@@ -1,44 +1,84 @@
-import React,{useContext, useState} from 'react'
-import NoteContext from '../Context/NoteContext';
+import React, { useContext, useState } from "react";
+import NoteContext from "../Context/NoteContext";
 
 export default function AddNote() {
-    const context = useContext(NoteContext);
-    // yaha pari ... ka use krke tino ka ek sath bhi change kr skte hai
-    const [Note, setNote] = useState({
-        title : "",
-        desc : "",
-        tag : ""
-    })
-    const Handle = (e) =>{
-        setNote({...Note,[e.target.id]:e.target.value});
-    }
-    const HandleSubmit = (e) => {
-        //e.preventDefault();
-        context.AddNote(Note.title,Note.desc,Note.tag);
-    }
+  const context = useContext(NoteContext);
+  // yaha pari ... ka use krke tino ka ek sath bhi change kr skte hai
+  const [Note, setNote] = useState({
+    title: "",
+    desc: "",
+    tag: "",
+  });
+  const Handle = (e) => {
+    setNote({ ...Note, [e.target.id]: e.target.value });
+  };
+  const HandleSubmit = (e) => {
+    context.AddNote(Note.title, Note.desc, Note.tag);
+    setNote({
+      title: "",
+      desc: "",
+      tag: "",
+    });
+    e.preventDefault();
+  };
 
-    return(
-        <div className="container my-3">
-            <h1>Add Note</h1>
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
-                    <input type="text" className="form-control" required onChange={Handle} id="title"  aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
-                    <textarea type="text" className="form-control" required rows={4} onChange={Handle} id="desc" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Tag</label>
-                    <input type="text" className="form-control" onChange={Handle} id="tag" />
-                </div>
-                {/* <div className="mb-3 form-check">
+  return (
+    <div className="container my-3">
+      <h1>Add Note</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={Handle}
+            id="title"
+            value={Note.title}
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            required
+            rows={4}
+            value={Note.desc}
+            onChange={Handle}
+            id="desc"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Tag
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={Handle}
+            value={Note.tag}
+            id="tag"
+          />
+        </div>
+        {/* <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                 </div> */}
-                <button disabled={Note.title === "" || Note.desc === ""} type="submit" className="btn btn-primary" onClick={HandleSubmit} >Add Note</button>
-            </form>
-        </div>
-    )
+        <button
+          disabled={Note.title === "" || Note.desc === ""}
+          type="submit"
+          className="btn btn-primary"
+          onClick={HandleSubmit}
+        >
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
 }
